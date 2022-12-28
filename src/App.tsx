a7d74fb750095d3adbdf64959ce96fd904f9d7c3
carlosandf <carlos.andres.ff2912@gmail.com>
2022-12-27 19:39:36 -0500

    Preparando el forntend de la aplicacion

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { Container, Button } from '@mui/material';
+
+function App() {
+
+  return (
+    <Container maxWidth="xl">
+      <Button variant='contained'>Hola Mundo</Button>
+    </Container>
+  );
+}
+
+export default App;
